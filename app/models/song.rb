49c2168c34ac2ba34857b2dcf15dfc8f6bc7e97a
SWFullPotential <swfullpotential@gmail.com>
2020-08-07 14:42:59 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
 
   def artist_name
+    self.artist ? self.artist.name : nil
   end
-
-  def artist_name=(name)
-  end
+    # could also be written as:
+    # if artist 
+    # artist.name 
+    # end
 end
